f5305528ac67ffb6894bd848249b00c3945e38ed
Marcel Hild <hild@b4mad.net>
2022-07-01 19:36:21 +0200

    wip

---
+++ src/components/Layout.tsx
@@ -0,0 +1,33 @@
+// inspired by https://github.com/mantinedev/mantine-gatsby-template/blob/master/src/components/Layout/Layout.tsx
+
+import React, { useState } from 'react';
+import { MantineProvider, ColorSchemeProvider, ColorScheme } from '@mantine/core';
+import { useHotkeys } from '@mantine/hooks';
+
+import { Nav } from "./homepage/Navbar";
+import { Footer } from "./homepage/Footer";
+
+interface LayoutProps {
+    children: React.ReactNode;
+}
+
+// export const Layout = ({ children }) => {
+export function Layout({ children }: LayoutProps) {
+
+    const [colorScheme, setColorScheme] = useState<ColorScheme>('light');
+    const toggleColorScheme = (value?: ColorScheme) =>
+        setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
+    useHotkeys([['mod+J', () => toggleColorScheme()]]);
+
+    return (
+        <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+            {/* Add your theme here */}
+            <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
+                <Nav />
+                {children}
+                <Footer />
+            </MantineProvider>
+        </ColorSchemeProvider>
+    );
+};
